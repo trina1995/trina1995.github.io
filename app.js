e05063c25b1795abd3086af102c5eb0be82b7e25
Trina Sahoo <trinasahoo95@gmail.com>
2026-01-03 17:30:36 +0100

    beautifies the entire code

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
+    // Button click transitions
+    document.querySelectorAll(".control").forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
-        })
+        });
     });
+
+    // Theme toggle
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+    });
 })();
